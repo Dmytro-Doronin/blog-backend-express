75f04a85de84530393143607f93b4f6145ca0fe0
Dmytro-Doronin <doronindmytro623@gmail.com>
2023-12-04 22:12:40 +0100

    post router

--- dist/app.js
+++ dist/app.js
@@ -8,10 +8,11 @@ const express_1 = __importDefault(require("express"));
 const videoRouter_1 = require("./routes/videoRouter");
 const blogsRouter_1 = require("./routes/blogsRouter");
 const deleteRouter_1 = require("./routes/deleteRouter");
+const postsRouter_1 = require("./routes/postsRouter");
 exports.app = (0, express_1.default)();
 exports.app.use(express_1.default.json());
 //endpoints
 exports.app.use('/testing/all-data', deleteRouter_1.deleteRouter);
 exports.app.use('/api', videoRouter_1.videoRouter);
-exports.app.use('/api/blogs', blogsRouter_1.blogsRouter);
-// app.use('/posts', videoRouter)
+exports.app.use('/blogs', blogsRouter_1.blogsRouter);
+exports.app.use('/posts', postsRouter_1.postsRouter);
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {videoRouter} from "./routes/videoRouter";
 import {blogsRouter} from "./routes/blogsRouter";
 import {deleteRouter} from "./routes/deleteRouter";
+import {postsRouter} from "./routes/postsRouter";
 
 export const app = express()
 
@@ -10,5 +11,5 @@ app.use(express.json())
 //endpoints
 app.use('/testing/all-data', deleteRouter)
 app.use('/api', videoRouter)
-app.use('/api/blogs', blogsRouter)
-// app.use('/posts', videoRouter)
+app.use('/blogs', blogsRouter)
+app.use('/posts', postsRouter)
