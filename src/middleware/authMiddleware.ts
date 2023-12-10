d8a2d8dfb3d1d3c742ee71bbb8f80faec791fc7d
Dmytro-Doronin <doronindmytro623@gmail.com>
2023-12-10 18:10:16 +0100

    fix auth

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 
 const login = 'admin'
-const password = 'querty'
+const password = 'qwerty'
 
 export const authMiddleware = (req: Request, res:Response, next: NextFunction) => {
     // if (req.headers['authorization'] !== 'Basic YWRtaW46cXVlcnR5') {
