675a286d46eeace0c10928906c6ce4b9649de2df
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-02-01 17:24:46 +0100

    v3

--- dist/routes/authRouter.js
+++ dist/routes/authRouter.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
+exports.registrationLimiter = exports.authRouter = void 0;
 const express_1 = require("express");
 const authController_1 = require("../controllers/authController");
 const blogsMiddleware_1 = require("../middleware/blogsMiddleware");
@@ -8,12 +8,19 @@ const loginValidationModel_1 = require("../validation/loginValidationModel");
 const authValidationModel_1 = require("../validation/authValidationModel");
 const authMiddlewareWithBearer_1 = require("../middleware/authMiddlewareWithBearer");
 const verifyTokenMiddleware_1 = require("../middleware/verifyTokenMiddleware");
-const rateLimitMiddleware_1 = require("../middleware/rateLimitMiddleware");
+const express_rate_limit_1 = require("express-rate-limit");
 exports.authRouter = (0, express_1.Router)();
+exports.registrationLimiter = (0, express_rate_limit_1.rateLimit)({
+    windowMs: 10 * 1000,
+    limit: 5,
+    message: 'Too many requests from this IP, please try again later.',
+    standardHeaders: true,
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 exports.authRouter.get('/me', authMiddlewareWithBearer_1.authMiddlewareWithBearer, authController_1.meController);
-exports.authRouter.post('/login', rateLimitMiddleware_1.rateLimitMiddleware, (0, loginValidationModel_1.loginValidationModelMiddleware)(), blogsMiddleware_1.errorMiddleware, authController_1.authController);
+exports.authRouter.post('/login', exports.registrationLimiter, (0, loginValidationModel_1.loginValidationModelMiddleware)(), blogsMiddleware_1.errorMiddleware, authController_1.authController);
 exports.authRouter.post('/logout', verifyTokenMiddleware_1.verifyTokenMiddleware, authController_1.logoutController);
 exports.authRouter.post('/refresh-token', verifyTokenMiddleware_1.verifyTokenMiddleware, authController_1.refreshTokenController);
-exports.authRouter.post('/registration', rateLimitMiddleware_1.rateLimitMiddleware, (0, authValidationModel_1.authRegistrationValidationMiddleware)(), blogsMiddleware_1.errorMiddleware, authController_1.registrationController);
-exports.authRouter.post('/registration-confirmation', rateLimitMiddleware_1.rateLimitMiddleware, (0, authValidationModel_1.authRegistrationConfirmationValidationMiddleware)(), blogsMiddleware_1.errorMiddleware, authController_1.registrationConfirmationController);
-exports.authRouter.post('/registration-email-resending', rateLimitMiddleware_1.rateLimitMiddleware, (0, authValidationModel_1.authEmailResendingValidationMiddleware)(), blogsMiddleware_1.errorMiddleware, authController_1.emailResendingController);
+exports.authRouter.post('/registration', exports.registrationLimiter, (0, authValidationModel_1.authRegistrationValidationMiddleware)(), blogsMiddleware_1.errorMiddleware, authController_1.registrationController);
+exports.authRouter.post('/registration-confirmation', exports.registrationLimiter, (0, authValidationModel_1.authRegistrationConfirmationValidationMiddleware)(), blogsMiddleware_1.errorMiddleware, authController_1.registrationConfirmationController);
+exports.authRouter.post('/registration-email-resending', exports.registrationLimiter, (0, authValidationModel_1.authEmailResendingValidationMiddleware)(), blogsMiddleware_1.errorMiddleware, authController_1.emailResendingController);
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -14,13 +14,22 @@ import {
 } from "../validation/authValidationModel";
 import {authMiddlewareWithBearer} from "../middleware/authMiddlewareWithBearer";
 import {verifyTokenMiddleware} from "../middleware/verifyTokenMiddleware";
-import {rateLimitMiddleware} from "../middleware/rateLimitMiddleware";
+import {rateLimit} from "express-rate-limit";
+
 export const authRouter = Router()
 
+export const registrationLimiter = rateLimit({
+    windowMs: 10 * 1000, // 10 секунд
+    limit: 5, // Максимальное количество запросов
+    message: 'Too many requests from this IP, please try again later.',
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 authRouter.get('/me', authMiddlewareWithBearer, meController)
-authRouter.post('/login', rateLimitMiddleware,loginValidationModelMiddleware(), errorMiddleware, authController)
+authRouter.post('/login', registrationLimiter, loginValidationModelMiddleware(), errorMiddleware, authController)
 authRouter.post('/logout',verifyTokenMiddleware, logoutController)
-authRouter.post('/refresh-token',verifyTokenMiddleware, refreshTokenController)
-authRouter.post('/registration', rateLimitMiddleware, authRegistrationValidationMiddleware(),  errorMiddleware, registrationController)
-authRouter.post('/registration-confirmation', rateLimitMiddleware, authRegistrationConfirmationValidationMiddleware(),  errorMiddleware,  registrationConfirmationController)
-authRouter.post('/registration-email-resending', rateLimitMiddleware, authEmailResendingValidationMiddleware(),  errorMiddleware,  emailResendingController)
+authRouter.post('/refresh-token', verifyTokenMiddleware, refreshTokenController)
+authRouter.post('/registration', registrationLimiter, authRegistrationValidationMiddleware(),  errorMiddleware, registrationController)
+authRouter.post('/registration-confirmation', registrationLimiter, authRegistrationConfirmationValidationMiddleware(),  errorMiddleware,  registrationConfirmationController)
+authRouter.post('/registration-email-resending', registrationLimiter, authEmailResendingValidationMiddleware(),  errorMiddleware,  emailResendingController)
