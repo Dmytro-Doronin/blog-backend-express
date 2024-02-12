32fcaaf1a02ea30f7da9214c27e9ae37cc825fb5
Dmytro-Doronin <doronindmytro623@gmail.com>
2024-02-12 12:15:32 +0100

    tests for devices

--- __tests__/e2e/device.e2e.ts
+++ __tests__/e2e/device.e2e.ts
@@ -3,13 +3,27 @@ import request = require('supertest')
 import {UsersInputModelType} from "../../src/types/commonBlogTypeAndPosts.types";
 import {createUserManager} from "../utils/createUserManager";
 import {sendEmailAndGetUserManager} from "../utils/sendEmailAndGetUserManager";
-describe('/comment', () => {
-    let userToken; // Хранение токена пользователя
-    let device1Id;
-    let device2Id;
-    let device3Id;
+import mongoose from "mongoose";
+import {url} from "../../src/db/db";
+// @ts-ignore
+import jwt from "jsonwebtoken";
+import {setting} from "../../src/variables";
+describe('/devices', () => {
+    let userToken: string; // Хранение токена пользователя
+    let device1Id: string;
+    let device2Id: string;
+    let device3Id: string;
+    let device4Id: string
+    //tokens
+    let refreshToken1: string
+    let refreshToken2: string
+    let refreshToken3: string
+    let refreshToken4: string
+
     beforeAll(async () => {
         // Создаем пользователя и получаем токен
+        await mongoose.connect(url)
+        await request(app).delete('/api/testing/all-data')
         const data: UsersInputModelType = {
             login: "Home",
             password: "123123",
@@ -34,21 +48,66 @@ describe('/comment', () => {
             .post('/api/auth/login')
             .send(loginData)
             .set('User-Agent', 'UserAgent1');
+            const cookies = loginResponse1.headers['set-cookie'];
+            const refreshTokenCookie = Array.isArray(cookies)
+             ? cookies.find((cookie) => cookie.startsWith('refreshToken='))
+             : undefined;
+
+             if (refreshTokenCookie) {
+                 refreshToken1 = refreshTokenCookie.split(';')[0].split('=')[1];
+                 // console.log('Refresh Token:', refreshToken1);
+             } else {
+                 console.log('Refresh Token not found in cookies.');
+             }
 
         const loginResponse2 = await request(app)
             .post('/api/auth/login')
             .send(loginData)
             .set('User-Agent', 'UserAgent2');
+        const cookies2 = loginResponse2.headers['set-cookie'];
+        const refreshTokenCookie2 = Array.isArray(cookies2)
+            ? cookies2.find((cookie) => cookie.startsWith('refreshToken='))
+            : undefined;
+
+        if (refreshTokenCookie2) {
+            refreshToken2 = refreshTokenCookie2.split(';')[0].split('=')[1];
+            // console.log('Refresh Token:', refreshToken1);
+        } else {
+            console.log('Refresh Token not found in cookies.');
+        }
+
 
         const loginResponse3 = await request(app)
             .post('/api/auth/login')
             .send(loginData)
             .set('User-Agent', 'UserAgent3');
+        const cookies3 = loginResponse3.headers['set-cookie'];
+        const refreshTokenCookie3 = Array.isArray(cookies3)
+            ? cookies3.find((cookie) => cookie.startsWith('refreshToken='))
+            : undefined;
+
+        if (refreshTokenCookie3) {
+            refreshToken3 = refreshTokenCookie3.split(';')[0].split('=')[1];
+            // console.log('Refresh Token:', refreshToken1);
+        } else {
+            console.log('Refresh Token not found in cookies.');
+        }
 
         const loginResponse4 = await request(app)
             .post('/api/auth/login')
             .send(loginData)
             .set('User-Agent', 'UserAgent4');
+        const cookies4 = loginResponse4.headers['set-cookie'];
+        const refreshTokenCookie4 = Array.isArray(cookies4)
+            ? cookies4.find((cookie) => cookie.startsWith('refreshToken='))
+            : undefined;
+
+        if (refreshTokenCookie4) {
+            refreshToken4 = refreshTokenCookie4.split(';')[0].split('=')[1];
+            // console.log('Refresh Token:', refreshToken1);
+        } else {
+            console.log('Refresh Token not found in cookies.');
+        }
 
         // Проверки на успешные логины, например:
         expect(loginResponse1.statusCode).toBe(200);
@@ -57,166 +116,151 @@ describe('/comment', () => {
         expect(loginResponse4.statusCode).toBe(200);
     });
 
+    it ('Get all devices', async () => {
+        const result = await request(app)
+            .get('/api/security/devices')
+            .set('Cookie', [`refreshToken=${refreshToken4}`])
 
 
+        device1Id = result.body[0].deviceId
+        device2Id = result.body[1].deviceId
+        device3Id = result.body[2].deviceId
+        device4Id = result.body[3].deviceId
+        expect(result.status).toBe(200);
+    })
 
-    // beforeEach(async () => {
-    //     await request(app).delete('/api/testing/all-data')
-    // }, 10000)
-
-
-
-    // it ('Should return devices', async () => {
-    //
-    //     await sendEmailAndGetUserManager()
-    //
-    //     const loginData = {
-    //         loginOrEmail: "Home",
-    //         password: "123123",
-    //     }
-    //     let refreshToken1
-    //     let refreshToken2
-    //     let refreshToken3
-    //     let refreshToken4
-    //      const device1 = await request(app)
-    //         .post('/api/auth/login')
-    //         .set('User-Agent', 'Mozilla/5.0')
-    //         .send(loginData)
-    //         .expect(200)
-    //          .then((response) => {
-    //              // Извлекаем значение refreshToken из заголовков ответа
-    //              const setCookieHeader = response.headers['set-cookie'];
-    //              const refreshTokenCookie = Array.isArray(setCookieHeader)
-    //                  ? setCookieHeader.find((cookie) => cookie.startsWith('refreshToken='))
-    //                  : undefined;
-    //
-    //              if (refreshTokenCookie) {
-    //                  refreshToken1 = refreshTokenCookie.split(';')[0].split('=')[1];
-    //                  // console.log('Refresh Token:', refreshToken1);
-    //              } else {
-    //                  console.log('Refresh Token not found in cookies.');
-    //              }
-    //          })
-    //
-    //     const device2 = await request(app)
-    //         .post('/api/auth/login')
-    //         .set('User-Agent', 'Mozilla/6.0')
-    //         .send(loginData)
-    //         .expect(200)
-    //         .then((response) => {
-    //             // Извлекаем значение refreshToken из заголовков ответа
-    //             const setCookieHeader = response.headers['set-cookie'];
-    //             const refreshTokenCookie = Array.isArray(setCookieHeader)
-    //                 ? setCookieHeader.find((cookie) => cookie.startsWith('refreshToken='))
-    //                 : undefined;
-    //
-    //             if (refreshTokenCookie) {
-    //                 refreshToken2 = refreshTokenCookie.split(';')[0].split('=')[1];
-    //                 // console.log('Refresh Token:', refreshToken2);
-    //             } else {
-    //                 console.log('Refresh Token not found in cookies.');
-    //             }
-    //         })
-    //
-    //     const device3 = await request(app)
-    //         .post('/api/auth/login')
-    //         .set('User-Agent', 'Mozilla/7.0')
-    //         .send(loginData)
-    //         .expect(200)
-    //         .then((response) => {
-    //             // Извлекаем значение refreshToken из заголовков ответа
-    //             const setCookieHeader = response.headers['set-cookie'];
-    //             const refreshTokenCookie = Array.isArray(setCookieHeader)
-    //                 ? setCookieHeader.find((cookie) => cookie.startsWith('refreshToken='))
-    //                 : undefined;
-    //
-    //             if (refreshTokenCookie) {
-    //                 refreshToken3 = refreshTokenCookie.split(';')[0].split('=')[1];
-    //                 // console.log('Refresh Token:', refreshToken3);
-    //             } else {
-    //                 console.log('Refresh Token not found in cookies.');
-    //             }
-    //         })
-    //
-    //
-    //
-    //     const device4 = await request(app)
-    //         .post('/api/auth/login')
-    //         .set('User-Agent', 'Mozilla/8.0')
-    //         .send(loginData)
-    //         .expect(200)
-    //         .then((response) => {
-    //             // Извлекаем значение refreshToken из заголовков ответа
-    //             const setCookieHeader = response.headers['set-cookie'];
-    //             const refreshTokenCookie = Array.isArray(setCookieHeader)
-    //                 ? setCookieHeader.find((cookie) => cookie.startsWith('refreshToken='))
-    //                 : undefined;
-    //
-    //             if (refreshTokenCookie) {
-    //                 refreshToken4 = refreshTokenCookie.split(';')[0].split('=')[1];
-    //                 // console.log('Refresh Token:', refreshToken4);
-    //             } else {
-    //                 console.log('Refresh Token not found in cookies.');
-    //             }
-    //         })
-    //
-    //     //check first time
-    //     const devices222 = await request(app)
-    //         .get('/api/security/devices')
-    //         .set('Cookie', [`refreshToken=${refreshToken2}`])
-    //         .expect(200)
-    //
-    //     expect(devices222.body).toHaveLength(4)
-    //
-    //     //update token
-    //     await request(app)
-    //         .post('/api/auth/refresh-token')
-    //         .set('Cookie', `refreshToken=${refreshToken1}`)
-    //         .expect(200)
-    //         .then((response) => {
-    //             // Извлекаем значение refreshToken из заголовков ответа
-    //             const setCookieHeader = response.headers['set-cookie'];
-    //             const refreshTokenCookie = Array.isArray(setCookieHeader)
-    //                 ? setCookieHeader.find((cookie) => cookie.startsWith('refreshToken='))
-    //                 : undefined;
-    //
-    //             if (refreshTokenCookie) {
-    //                 refreshToken1 = refreshTokenCookie.split(';')[0].split('=')[1];
-    //             } else {
-    //                 console.log('Refresh Token not found in cookies.');
-    //             }
-    //         })
-    //
-    //     //check second time
-    //     const devices23 = await request(app)
-    //         .get('/api/security/devices')
-    //         .set('Cookie', [`refreshToken=${refreshToken1}`])
-    //         .expect(200)
-    //
-    //     console.log(devices23.body)
-    //     // //
-    //     // const devices1 = await request(app)
-    //     //     .get('/api/security/devices')
-    //     //     .set('Cookie', [`refreshToken=${refreshToken1}`])
-    //     //     .expect(200)
-    //
-    //     // console.log(devices1.body)
-    //     // const deletedDevice2 = devices23.body[1].deviceId
-    //     // await request(app)
-    //     //     .delete(`/api/security/devices/${deletedDevice2}`)
-    //     //     .set('Cookie', `refreshToken=${refreshToken1}`)
-    //     //     .expect(204);
-    //     //
-    //     //
-    //     // const devices233 = await request(app)
-    //     //     .get('/api/security/devices')
-    //     //     .set('Cookie', [`refreshToken=${refreshToken1}`])
-    //     //     .expect(200)
-    //     //
-    //     // console.log(devices233.body)
-    //
-    //
-    // })
+    it ('RefreshToken inside cookie is missing', async () => {
+        const result = await request(app)
+            .get('/api/security/devices')
 
 
+        expect(result.status).toBe(401);
+    })
+
+    it ('RefreshToken inside cookie expired', async () => {
+
+        let result
+        try {
+            const decodedToken = jwt.verify(refreshToken4, setting.JWT_SECRET) as jwt.JwtPayload;
+
+            if (!decodedToken || typeof decodedToken.exp !== 'number') {
+                console.log('Некорректный refreshToken');
+                // Обработка некорректного refreshToken
+                return;
+            }
+
+            const tokenExpirationTimeInSeconds = decodedToken.exp;
+            const currentTimeInSeconds = Math.floor(Date.now() / 1000);
+            const oneHourInSeconds = 3601; // 1 час = 60 минут * 60 секунд
+
+            const expirationTime = currentTimeInSeconds + oneHourInSeconds;
+
+
+            if (tokenExpirationTimeInSeconds < expirationTime) {
+                // refreshToken истек
+                console.log('refreshToken expired');
+            } else {
+                // refreshToken еще действителен
+                console.log('refreshToken is valid');
+            }
+        } catch (error) {
+            // Ошибка при декодировании токена
+            console.error('Ошибка при декодировании refreshToken:', (error as Error).message);
+        }
+
+        expect(result === 'refreshToken expired')
+    })
+
+    it ('Update refreshToken for device 1', async () => {
+
+        let before
+        let after
+
+        const resultBeforeChangeToken = await request(app)
+            .get('/api/security/devices')
+            .set('Cookie', [`refreshToken=${refreshToken1}`])
+
+        before = resultBeforeChangeToken.body
+
+        const change = await request(app)
+            .post('/api/auth/refresh-token')
+            .set('Cookie', [`refreshToken=${refreshToken1}`])
+
+        const cookies = change.headers['set-cookie'];
+        const refreshTokenCookie = Array.isArray(cookies)
+            ? cookies.find((cookie) => cookie.startsWith('refreshToken='))
+            : undefined;
+
+        if (refreshTokenCookie) {
+            refreshToken1 = refreshTokenCookie.split(';')[0].split('=')[1];
+            // console.log('Refresh Token:', refreshToken1);
+        } else {
+            console.log('Refresh Token not found in cookies.');
+        }
+
+        expect(change.status).toBe(200)
+
+        const resultAfterChangeToken = await request(app)
+            .get('/api/security/devices')
+            .set('Cookie', [`refreshToken=${refreshToken2}`])
+
+        after = resultAfterChangeToken.body
+
+
+        expect(after.length).toEqual(before.length);
+        expect(after[0].deviceId).toEqual(before[0].deviceId);
+        expect(after[1].deviceId).toEqual(before[1].deviceId);
+        expect(after[2].deviceId).toEqual(before[2].deviceId);
+        expect(after[3].deviceId).toEqual(before[3].deviceId);
+        expect(after[0].lastActiveDate).not.toBe(before[0].lastActiveDate);
+    })
+
+    it ('Delete device 2', async () => {
+         const delRes = await request(app)
+            .delete(`/api/security/devices/${device2Id}`)
+            .set('Cookie', [`refreshToken=${refreshToken1}`])
+
+        // expect(delRes.status).toBe(204)
+
+        const res = await request(app)
+            .get('/api/security/devices')
+            .set('Cookie', [`refreshToken=${refreshToken3}`])
+
+        expect(res.body).toHaveLength(3);
+    })
+
+    it ('LogOut by device 3', async () => {
+        const logRes = await request(app)
+            .post(`/api/auth/logout`)
+            .set('Cookie', [`refreshToken=${refreshToken3}`])
+
+        expect(logRes.status).toBe(204)
+        // expect(delRes.status).toBe(204)
+
+        const res = await request(app)
+            .get('/api/security/devices')
+            .set('Cookie', [`refreshToken=${refreshToken1}`])
+
+        expect(res.body).toHaveLength(2);
+    })
+
+    it ('Delete other devices (must left only 1 device)', async () => {
+           const delRes =  await request(app)
+                .delete('/api/security/devices')
+                .set('Cookie', [`refreshToken=${refreshToken1}`])
+
+        expect(delRes.status).toBe(204)
+        // expect(delRes.status).toBe(204)
+
+        const res = await request(app)
+            .get('/api/security/devices')
+            .set('Cookie', [`refreshToken=${refreshToken1}`])
+
+        console.log(res.body)
+        expect(res.body).toHaveLength(1);
+    })
+
+    afterAll(async () => {
+        await mongoose.connection.close()
+    })
 })
